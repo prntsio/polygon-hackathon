2200f5e57ccee0f132066737bf23f3d8cef4adda
0xThresh <0xthresh@protonmail.com>
2022-08-18 20:11:42 -0700

    Added documentation to code

--- lambda/send_tokens.py
+++ lambda/send_tokens.py
@@ -10,7 +10,7 @@ def lambda_handler(event, context):
     # Operator address from contract
     operator_address = "0x739c0b2413B8778Dfd2B8cBA58b1FAc12A3AEb6E"
     
-    w3, contract = configure_web3_provider() # both need to be fed into both other functions
+    w3, contract = configure_web3_provider() 
     
     participant = check_participant_status(transfer_address, contract)
     
@@ -20,6 +20,7 @@ def lambda_handler(event, context):
             'statusCode': 403,
             'body': json.dumps("Wallet is not an authorized participant according to the contract.")
         }
+        
     elif participant == True:
         print("Wallet is a valid participant, sending tokens...")
         txn = send_tokens(w3, transfer_address, contract, operator_address)
@@ -32,48 +33,96 @@ def lambda_handler(event, context):
         return {
             'statusCode': 200,
             'body': json.dumps(f'Tokens sent successfully, transaction ID: {txn_id}')
-    }
+        }
+        
+    else:
+        print("Unknown error; most likely reason is that an invalid wallet address was sent in the event.")
+        return {
+            'statusCode': 403,
+            'body': json.dumps("Unknown error; most likely reason is that an invalid wallet address was sent in the event.")
+        }
    
     
 def configure_web3_provider():
-    # Configures web3 provider to have access to private key
-    # This is required, since the lambda function will act as the operator
-    # Extra special care needs to be taken to ensure that no one can publicly
-    # take over this Lambda function to start using my wallet in unintended manners
+    """Configures a web3 provider and contract object
     
-    # line below proves that the MATIC network client is in use 
-    #print(w3.clientVersion)
-    
+    :param infura_id: ID of the Infura project used for access to blockchains
+    :type infura_id: str 
+    :param w3: provider object created using Web3 library 
+    :type w3: <class 'web3.main.Web3'>
+    :param abi: contract ABIs loaded from a file 
+    :type abi: dict 
+    :returns: a w3 provider object and a contract object
+    :param contract: contract object used to make calls and transactions to the contract on Polygon 
+    :type contract: <class 'web3._utils.datatypes.Contract'>
+    :rtype: w3, contract 
+    """
     
     infura_id = os.environ.get("INFURA_ID")
-    w3 = Web3(Web3.HTTPProvider(f'https://polygon-mainnet.infura.io/v3/{infura_id}'))
-
     
+    # creates the web3 provider using the Infura backend
+    w3 = Web3(Web3.HTTPProvider(f'https://polygon-mainnet.infura.io/v3/{infura_id}'))
+    
+    # required for non-ETH blockchains where extraData field > 32 bytes
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
     
+    # loads contract ABIs into a variable
     with open("abi.json") as f:
         abi = json.load(f)
     
+    # creates contract object, with callable functions and contract address on Polygon
     contract = w3.eth.contract(address='0x4F14BAC02d7d8a0219839EC36fd5158489aaEBA5', abi=abi)
     
     return w3, contract
     
     
 def check_participant_status(_transfer_address, _contract): 
+    """Calls the getParticipantStatus function from the contract 
+    
+    Calls the getParticipantStatus function from the contract for use in the 
+    primary function, to ensure that the specified wallet is a valid participant
+    and has opted in to receiving tokens. 
+    
+    :param participant_status: Current participant status stored in the contract for the wallet address 
+    :type participant_status: bool 
+    :returns: the bool value of the wallet's particpant status 
+    :rtype: bool 
+    """
     
     participant_status = _contract.functions.getParticipantStatus(_transfer_address).call()
+    
     return participant_status
     
 
 
 def send_tokens(_w3, _transfer_address, _contract, _operator_address):
-    # Insert web3.py call to send tokens here
-    # Returns transaction ID as string
+    """Sends tokens to the wallet address received in the event
     
-    # Current nonce for the operator address
+    Sends tokens to the wallet address received in the event, as long as the
+    wallet is a valid participant and has opted in to receving tokens. The 
+    logic in lambda_handler is responsible for ensuring that a wallet is a valid
+    participant prior to this function being called. 
+    
+    :param nonce: current nonce value of the wallet calling the transfer() function 
+    :type nonce: int 
+    :param transaction_parameters: parameters passed into the transaction sent to the contract 
+    :type transaction_parameters: dict 
+    :param private_key: private key of the operator address
+    :type private_key: str
+    :param transfer_txn: the transaction that transfers tokens from the contract to the receving wallet 
+    :type transfer_txn: dict
+    :param signed_txn: transaction signed using private key, giving it the ability to be sent 
+    :type signed_txn: <class 'eth_account.datastructures.SignedTransaction'>
+    :param txn: hex data of sent transaction
+    :type txn: <class 'hexbytes.main.HexBytes'>
+    :param txn_receipt: receipt object that includes data about transaction, such as transaction ID
+    :type txn_receipt: <class 'hexbytes.main.HexBytes'>
+    :returns: txn_receipt, which is used to extract the transaction ID 
+    :rtype: <class 'hexbytes.main.HexBytes'>
+    """
+    
     nonce = _w3.eth.getTransactionCount(_operator_address)
     
-    # Parameters required to send transaction 
     transaction_parameters = {
         'from': _operator_address,
         'nonce': nonce
@@ -81,12 +130,14 @@ def send_tokens(_w3, _transfer_address, _contract, _operator_address):
     
     # Private key from Lambda env vars
     private_key = os.environ.get("PRIVATE_KEY")
-    assert private_key is not None, "You must set PRIVATE_KEY environment variable"
+    assert private_key is not None, "PRIVATE_KEY environment variable is not set; cannot send transaction without it"
     
     transfer_txn = _contract.functions.transfer(_operator_address, _transfer_address, 1).build_transaction(transaction_parameters)
-    signed_txn = _w3.eth.account.signTransaction(transfer_txn, private_key=private_key)
-    txn = _w3.eth.sendRawTransaction(signed_txn.rawTransaction)
-    tx_receipt = _w3.eth.wait_for_transaction_receipt(txn)
-
-    return tx_receipt
     
+    signed_txn = _w3.eth.account.signTransaction(transfer_txn, private_key=private_key)
+    
+    txn = _w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    
+    txn_receipt = _w3.eth.wait_for_transaction_receipt(txn)
+
+    return txn_receipt
